9033a1ebb6c46e0c49a478ee2d5aded541f1ddbe
td0 <tadho@me.com>
2018-10-19 04:24:03 +0700

    Hello Pig!
    Signed-off-by: td0 <tadho@me.com>

---
+++ assets/js/poogiesScript.js
@@ -0,0 +1,33 @@
+for (var i = 0; i < orderedPoogies.length; i++) {
+  if (orderedPoogies[i].desc !== '-') orderedPoogies[i].region = `<strong>${orderedPoogies[i].region}</strong>
+    <br /><p class="loc-desc">${orderedPoogies[i].desc}</p>`;
+  else orderedPoogies[i].region = `<strong>${orderedPoogies[i].region}</strong>`;
+  orderedPoogies[i].maps = '<img src="'+orderedPoogies[i].maps+'" />'
+}
+
+$(document).ready( function () {
+    $('#poogies_table').DataTable({
+      data: orderedPoogies,
+      processing: true,
+      columns: [
+        {
+          data: "number",
+          width: "2%"
+        },
+        {data: "name"},
+        {
+          data: "region",
+          className: "td-region",
+          width: "45%"
+        },
+        {
+          data: "maps",
+          orderable: false
+        }
+      ],
+      columnDefs: [{
+        targets: [ 0, 1],
+        className: 'mdl-data-table__cell--non-numeric'
+      }]
+    });
+} );
