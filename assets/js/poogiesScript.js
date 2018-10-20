66913f7d474cdde2203246e8175006b406e36702
td0 <tadho@me.com>
2018-10-20 20:01:15 +0700

    minor fix
    Signed-off-by: td0 <tadho@me.com>

--- assets/js/poogiesScript.js
+++ assets/js/poogiesScript.js
@@ -1,13 +1,18 @@
 for (var i = 0; i < orderedPoogies.length; i++) {
-  if (orderedPoogies[i].desc !== '-') orderedPoogies[i].region = `<strong>${orderedPoogies[i].region}</strong>
-    <br /><p class="loc-desc">${orderedPoogies[i].desc}</p>`;
-  else orderedPoogies[i].region = `<strong>${orderedPoogies[i].region}</strong>`;
+  if (i<9) orderedPoogies[i].number = '00'+(i+1)
+  else if (i<99)  orderedPoogies[i].number = '0'+(i+1)
+  orderedPoogies[i].name = `<strong>${orderedPoogies[i].name}</strong>`
   orderedPoogies[i].maps = '<img src="'+orderedPoogies[i].maps+'" />'
+  if (orderedPoogies[i].desc !== '-') orderedPoogies[i].region = `<strong><u>${orderedPoogies[i].region}</u></strong>
+    <br />${orderedPoogies[i].maps}
+    <br /><p class="loc-desc">${orderedPoogies[i].desc}</p>`;
+  else orderedPoogies[i].region = `<strong><u>${orderedPoogies[i].region}</u></strong><br />${orderedPoogies[i].maps}`;
 }
 
 $(document).ready( function () {
     $('#poogies_table').DataTable({
       data: orderedPoogies,
+      responsive: true,
       scrollY: '72vh',
       scrollX: true,
       paging: false,
@@ -21,10 +26,6 @@ $(document).ready( function () {
           data: "region",
           className: "td-region",
           width: "45%"
-        },
-        {
-          data: "maps",
-          orderable: false
         }
       ],
       columnDefs: [{
